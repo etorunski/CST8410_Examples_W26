12d2467a3c8147f8f0c339a53119f98b57c26dc6
etorunski <etorunski@gmail.com>
2026-02-03 17:10:39 -0500

    finished EncryptedSharedPreferences

--- app/src/main/java/com/example/mylabs/MainActivity.kt
+++ app/src/main/java/com/example/mylabs/MainActivity.kt
@@ -1,10 +1,6 @@
 package com.example.mylabs
 
-import android.R.attr.label
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
+
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -24,6 +20,7 @@ import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -35,12 +32,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
 import com.example.mylabs.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity  (){ // means call constructor from parent
@@ -97,8 +97,42 @@ class MainActivity : ComponentActivity  (){ // means call constructor from paren
 //this is our function:
 @Composable
 fun DisplayText(modifier: Modifier = Modifier) {
-                    //store the value for next call
-    var currentValue = remember { mutableStateOf("Hello world") }
+
+    val context = LocalContext.current
+    val mainKey = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)//get the cryptographic key
+
+    val sharedPreferences = EncryptedSharedPreferences.create(
+        "MyFileName" ,
+        mainKey,
+        context,
+        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+    )
+
+
+    //store the value for next call
+    var currentValue = remember { mutableStateOf(sharedPreferences.getString("USER_INPUT", "").orEmpty() ) }
+    var agreeCollectData = remember{mutableStateOf(false) }
+    var isShowingDialog = remember { mutableStateOf(true) }
+
+    if (isShowingDialog.value)
+        AlertDialog(
+            onDismissRequest = { }, //this gets called when you click outside the dialog
+            title = { Text(text = "Dialog Title") },
+            text = { Text("Here is a text ") },
+            confirmButton = {       //This below causes a recomposition
+                Button(onClick = {
+                    isShowingDialog.value = false
+                    agreeCollectData.value = true
+                }) {
+                    Text("Ok")
+                }
+            },
+            dismissButton = {   //This below causes a recomposition
+                Button(onClick = { isShowingDialog.value = false }) {
+                    Text("Cancel")
+                }
+            })
 
     Column(
         modifier=modifier.fillMaxHeight(),//.background(Color.Yellow),
@@ -112,11 +146,23 @@ fun DisplayText(modifier: Modifier = Modifier) {
         )
         TextField( value = currentValue.value,
             onValueChange = { newText ->//user types something in:
-                currentValue.value = newText}, //changing, do redraw
+            run {
+                if(agreeCollectData.value)
+                    with(sharedPreferences.edit())
+                    {
+                        putString("USER_INPUT", newText)
+                        apply()  // apply the changes to disk
+                    }
+
+                currentValue.value = newText
+            }
+                            }, //changing, do redraw
                 label = { Text("Type below") },
                 placeholder = { Text("Type here") }
             )
     }
+
+
 } //variable disappears
 
 @Preview(showBackground = true)
